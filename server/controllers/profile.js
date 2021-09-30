000a81f4f577402e7783b743a77988540e499a38
Gamey001 <gamalieldashua@gmail.com>
2021-09-30 18:43:14 +0100

    made changes to the profile controllers

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -5,25 +5,24 @@ const asyncHandler = require("express-async-handler");
 // @desc Create new profile
 // @access Public
 exports.createProfile = asyncHandler(async (req, res, next) => {
-  const {
-    firstName,
-    lastName,
-    gender,
-    birthDate,
-    email,
-    phoneNumber,
-    location,
-    profilePic,
-    description,
-    availability,
-  } = req.body;
-
   try {
+    const {
+      firstName,
+      lastName,
+      gender,
+      birthday,
+      email,
+      phoneNumber,
+      location,
+      profilePic,
+      description,
+      availability,
+    } = req.body;
     const profile = await Profile.create({
       firstName,
       lastName,
       gender,
-      birthDate,
+      birthday,
       email,
       phoneNumber,
       location,
@@ -32,7 +31,6 @@ exports.createProfile = asyncHandler(async (req, res, next) => {
       availability,
     });
 
-    profile.save();
     res.status(200).json({
       profile,
     });
@@ -46,96 +44,73 @@ exports.createProfile = asyncHandler(async (req, res, next) => {
 // @desc update a profile with given ID
 // @access Private
 exports.updateProfile = asyncHandler(async (req, res, next) => {
-  const id = req.user._id;
+  try {
+    const id = req.user._id;
 
-  const {
-    firstName,
-    lastName,
-    gender,
-    birthDate,
-    email,
-    phoneNumber,
-    location,
-    profilePic,
-    description,
-    availability,
-  } = req.body;
-  User.findByIdAndUpdate(
-    id,
-    {
-      firstName: firstName,
-      lastName: lastName,
-      gender: gender,
-      birthDate: birthDate,
-      phoneNumber: phoneNumber,
-      location: address,
-      description: description,
-    },
-    { new: true },
-    function (err, user) {
-      if (err) {
-        res.status(404);
-        throw new Error("Something went wrong, please try again");
-      } else {
-        res.json({
-          user: {
-            id,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            gender: user.gender,
-            birthDate: user.birthDate,
-            email: user.email,
-            phoneNumber: user.phoneNumber,
-            location: user.location,
-            description: user.description,
-          },
-        });
-      }
-    }
-  );
+    const {
+      firstName,
+      lastName,
+      gender,
+      birthday,
+      email,
+      phoneNumber,
+      location,
+      profilePic,
+      description,
+      availability,
+    } = req.body;
+    const profile = await Profile.findByIdAndUpdate(
+      id,
+      {
+        firstName,
+        lastName,
+        gender,
+        birthday,
+        email,
+        phoneNumber,
+        location,
+        profilePic,
+        description,
+        availability,
+      },
+      { new: true }
+    );
+    res.status(200).json({
+      profile,
+    });
+  } catch (err) {
+    res.status(400);
+    throw new Error("Something went wrong, please try again");
+  }
 });
 
 // @route GET /profile/:id
 // @desc update a profile with the given ID and parameters
 // @access Private
 exports.getProfile = asyncHandler(async (req, res, next) => {
-  const id = req.user._id;
-
-  const user = await User.findById({ id });
-
-  User.findById(id, function (err, user) {
-    if (err) {
-      res.status(204);
-      throw new Error("Something went wrong, please try again");
-    } else {
-      res.status(200).json({
-        user: {
-          id,
-          username: user.username,
-          firstName: user.firstName,
-          lastName: user.lastName,
-          email: user.email,
-          gender: user.gender,
-          location: user.location,
-          phoneNumber: user.phoneNumber,
-          profilePic: user.profilePic,
-          birthDate: user.birthDate,
-          description: user.description,
-        },
-      });
-    }
-  });
+  try {
+    const id = req.user._id;
+    const profile = await Profile.findById({ id });
+    res.status(200).json({
+      profile,
+    });
+  } catch (err) {
+    res.status(400);
+    throw new Error("Something went wrong, please try again");
+  }
 });
 
 // @route GET /profile
 // @desc get all profiles
 // @access Private
 exports.getAllProfiles = asyncHandler(async (req, res, next) => {
-  User.find({}, "-password", function (err, profiles) {
-    if (err) {
-      res.status(400);
-      throw new Error("Something went wrong, please try again");
-    }
-    res.status(200).json(profiles);
-  });
+  try {
+    const profiles = await Profile.find();
+    res.status(200).json({
+      profiles,
+    });
+  } catch (err) {
+    res.status(400);
+    throw new Error("Something went wrong, please try again");
+  }
 });
