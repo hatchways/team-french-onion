d572c50d8482f7d2e4800da597ff3e29d0c70382
Gamey001 <gamalieldashua@gmail.com>
2021-10-02 19:02:22 +0100

    Update validate.js

--- server/validate.js
+++ server/validate.js
@@ -1,8 +1,8 @@
 const { check, validationResult } = require("express-validator");
 
 const msgPrefix = "Please enter a valid";
-const genderOptions = ['MALE', 'FEMALE', 'OTHER'];
-const checkFalsyVal = {checkFalsy: true}
+const genderOptions = ["MALE", "FEMALE", "OTHER"];
+const checkFalsyVal = { checkFalsy: true };
 
 exports.validateRegister = [
   check("username", `${msgPrefix} username`).notEmpty(),
@@ -11,7 +11,7 @@ exports.validateRegister = [
     "password",
     "Please enter a password with 6 or more characters"
   ).isLength({
-    min: 6
+    min: 6,
   }),
   (req, res, next) => {
     const errors = validationResult(req);
@@ -20,7 +20,7 @@ exports.validateRegister = [
     if (!errors.isEmpty())
       return res.status(400).json({ errors: errors.array() });
     next();
-  }
+  },
 ];
 
 exports.validateLogin = [
@@ -32,27 +32,58 @@ exports.validateLogin = [
     if (!errors.isEmpty())
       return res.status(400).json({ errors: errors.array() });
     next();
-  }
+  },
 ];
 
 exports.validateProfileDetails = [
-  check("firstName", ` ${msgPrefix} first name`).exists(checkFalsyVal).trim().escape().isLength({max: 20}),
-  check("lastName", ` ${msgPrefix} last name`).exists(checkFalsyVal).trim().escape().isLength({max: 20}),
-  check("gender", ` ${msgPrefix} gender`).exists(checkFalsyVal).isIn(genderOptions),
-  check("birthday", ` ${msgPrefix} birthday format`).exists(checkFalsyVal).toDate(),
-  check("email", ` ${msgPrefix} email address`).exists(checkFalsyVal).trim().escape().isEmail(),
-  check("phoneNumber", ` ${msgPrefix} phone number`).exists(checkFalsyVal).trim().isMobilePhone(),
-  check("location", ` ${msgPrefix} location`).exists(checkFalsyVal).trim().escape().isLength({max: 10}),
-  check("profilePic", ` ${msgPrefix} image url`).exists(checkFalsyVal).escape().isURL(),
-  check("description", ` ${msgPrefix} description format`).exists(checkFalsyVal).escape().isLength({max: 100}),
-  check("availability", ` ${msgPrefix} availability format`).exists(checkFalsyVal).isArray(),
+  check("firstName", ` ${msgPrefix} first name`)
+    .exists(checkFalsyVal)
+    .trim()
+    .escape()
+    .isLength({ max: 20 }),
+  check("lastName", ` ${msgPrefix} last name`)
+    .exists(checkFalsyVal)
+    .trim()
+    .escape()
+    .isLength({ max: 20 }),
+  check("gender", ` ${msgPrefix} gender`)
+    .exists(checkFalsyVal)
+    .isIn(genderOptions),
+  check("birthday", ` ${msgPrefix} birthday format`)
+    .exists(checkFalsyVal)
+    .toDate(),
+  check("email", ` ${msgPrefix} email address`)
+    .exists(checkFalsyVal)
+    .trim()
+    .escape()
+    .isEmail(),
+  check("phoneNumber", ` ${msgPrefix} phone number`)
+    .exists(checkFalsyVal)
+    .trim()
+    .isMobilePhone(),
+  check("location", ` ${msgPrefix} location`)
+    .exists(checkFalsyVal)
+    .trim()
+    .escape()
+    .isLength({ max: 10 }),
+  check("profilePic", ` ${msgPrefix} image url`)
+    .exists(checkFalsyVal)
+    .escape()
+    .isURL(),
+  check("description", ` ${msgPrefix} description format`)
+    .exists(checkFalsyVal)
+    .escape()
+    .isLength({ max: 100 }),
+  check("availability", ` ${msgPrefix} availability format`)
+    .exists(checkFalsyVal)
+    .isArray(),
   (req, res, next) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty())
       return res.status(400).json({ errors: errors.array() });
     next();
-  }
+  },
 ];
 
 exports.validateProfileId = [
@@ -63,5 +94,5 @@ exports.validateProfileId = [
     if (!errors.isEmpty())
       return res.status(400).json({ errors: errors.array() });
     next();
-  }
+  },
 ];
