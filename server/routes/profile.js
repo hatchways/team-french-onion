50311bfb232ea480ab2c844ff68bdb3cd4d60281
Gamey001 <gamalieldashua@gmail.com>
2021-10-03 05:08:55 +0100

    Create profile.js

---
+++ server/routes/profile.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const protect = require("../middleware/auth");
+const {validateProfileId, validateProfileDetails} = require("../validate")
+const router = express.Router();
+const {
+  updateProfile,
+  getProfile,
+  getAllProfiles,
+} = require("../controllers/profile");
+
+router.route("/create").post(protect, validateProfileDetails, createProfile);
+
+router.route("/update").put(protect, validateProfileId, validateProfileDetails, updateProfile);
+
+router.route("/").get(protect, getProfile);
+
+router.route("/profiles").get(protect, getAllProfiles);
+
+module.exports = router;
