ebb0449af7ad4db090be0abb6233448144a13be7
Juan D <juadel@hotmail.com>
2021-10-03 13:30:02 -0700

    prettier changes

--- server/models/Request.js
+++ server/models/Request.js
@@ -1,38 +1,37 @@
 const mongoose = require("mongoose");
 
 const requestSchema = new mongoose.Schema({
-    userId:{
-        type: String,
-        required: true,
+  userId: {
+    type: String,
+    required: true,
+  },
+  sitterId: {
+    type: String,
+    required: true,
+  },
+  start: {
+    type: Date,
+    required: true,
+  },
+  end: {
+    type: Date,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return v > this.start;
       },
-    sitterId:{
-        type: String,
-        required: true,
+      message: "End Date must later than Start Date",
     },
-    start:{
-        type: Date,
-        required: true
-    },
-    end:{
-        type: Date,
-        required: true,
-        validate: {
-            validator: function(v){
-            return v > this.start
-            },
-            message: "End Date must later than Start Date"
-        }
-    },
-    status:{
-        type: String,
-        enum : ["accepted", "declined", "pending"],
-        default: "pending"
-    },
-    paid:{
-        type: Boolean,
-        default: false
-    }
+  },
+  status: {
+    type: String,
+    enum: ["accepted", "declined", "pending"],
+    default: "pending",
+  },
+  paid: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = Request = mongoose.model("Request", requestSchema);
-
