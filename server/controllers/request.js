46ef5ab5322d3536a2de43602d783b86e9a420f2
Juan D <juadel@hotmail.com>
2021-10-03 13:42:33 -0700

    updating controllers using req.user.id

--- server/controllers/request.js
+++ server/controllers/request.js
@@ -1,60 +1,61 @@
 const Request = require("../models/Request");
 const asyncHandler = require("express-async-handler");
 
-// @route GET /requests?userid={}
+// @route GET /requests
 // @desc Search for request of especific userId
 // @access Private
 
 exports.getRequests = asyncHandler(async (req, res, next) => {
-    const userId = req.query.userid;
-    
-    let requests
-    if (userId) {
-      requests = await Request.find({
-        userId: userId
-      });
-    }
-  
-    res.status(200).json({ requests: requests });
-  });
+  const userId = req.user.id;
 
-// @route POST /requests/new
-// @desc Post a new request 
+  const requests = await Request.find({ userId: userId });
+  res.status(200).json({ requests: requests });
+});
+
+// @route POST /requests
+// @desc Post a new request
 // @access Private
 
 exports.postRequest = asyncHandler(async (req, res, next) => {
-    const { userId, sitterId, start, end, status, paid } = req.body;
-    
-    const newRequest = await Request.create({
-        userId,
-        sitterId,
-        start,
-        end,
-        status,
-        paid
-      });
-   
+  const userId = req.user.id;
+  const { sitterId, start, end, status, paid } = req.body;
 
-    res.status(201).json({ success: {
-      newRequest: {id: newRequest._id,}
-      }});
+  const newRequest = await Request.create({
+    userId,
+    sitterId,
+    start,
+    end,
+    status,
+    paid,
+  });
 
+  res.status(201).json({
+    success: {
+      newRequest: { id: newRequest._id },
+    },
+  });
 });
 
-// @route PATH /:requestid
+// @route PATH /requests/:requestid
 // @desc Update request status
 // @access Private
 
 exports.updateRequest = asyncHandler(async (req, res, next) => {
-    const requestId = req.params.requestid;
-    const status = req.body.status;
-    
-    const updatedRequest = await Request.findOne({_id : requestId});
-    updatedRequest.status = status;
-   
-    await updatedRequest.save();
+  const requestId = req.params.requestid;
+  const status = req.body.status;
+  const userId = req.user.id;
 
-    res.status(200).json({ success: {
-      updatedRequest: updatedRequest
-      }});
+  const updatedRequest = await Request.findOne({ _id: requestId });
+  if (updatedRequest.userId == sitterId) {
+    updatedRequest.status = status;
+    await updatedRequest.save();
+    res.status(200).json({
+      success: {
+        updatedRequest: updatedRequest,
+      },
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user Id data");
+  }
 });
---
+++ server/models/Request.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const RequestSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  sitterId: {
+    type: String,
+    required: true,
+  },
+  start: {
+    type: Date,
+    required: true,
+  },
+  end: {
+    type: Date,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return v > this.start;
+      },
+      message: "End Date must later than Start Date",
+    },
+  },
+  status: {
+    type: String,
+    enum: ["accepted", "declined", "pending"],
+    default: "pending",
+  },
+  paid: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = RequestModel = mongoose.model("request", RequestSchema);
