8b4e068baca01cb8fee334cb4deac33ed3b6c8f0
Gamey001 <gamalieldashua@gmail.com>
2021-10-03 06:29:48 +0100

    Create Notification.js

---
+++ server/models/Notification.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema({
+  senderEmail: {
+      type: String,
+      required: true,
+      unique: true,
+      ref: 'User'
+  },
+  receipientEmail: {
+    type: String,
+    required: true,
+    unique: true,
+    ref: 'User'
+  },
+  type: {
+    type: String,
+    required: true,
+    enum: ['message', 'request'],
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  read:{
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  date: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+});
+
+notificationSchema.methods.toggleReadStatus = function () {
+ this.read = !this.read;
+ this.save();
+};
+
+module.exports = Notification = mongoose.model("notification", notificationSchema);
