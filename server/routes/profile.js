9196f826b87e45eb3cceee8b816db6fbfe712167
Gamey001 <gamalieldashua@gmail.com>
2021-10-04 14:01:50 +0100

    Update profile.js

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const protect = require("../middleware/auth");
-const {validateProfileId, validateProfileDetails} = require("../validate")
+const { validateProfileId, validateProfileDetails } = require("../validate");
 const router = express.Router();
 const {
   updateProfile,
@@ -10,7 +10,9 @@ const {
 
 router.route("/create").post(protect, validateProfileDetails, createProfile);
 
-router.route("/update").put(protect, validateProfileId, validateProfileDetails, updateProfile);
+router
+  .route("/update")
+  .put(protect, validateProfileId, validateProfileDetails, updateProfile);
 
 router.route("/").get(protect, getProfile);
 
