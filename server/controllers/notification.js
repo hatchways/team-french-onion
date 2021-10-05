e8a5d0b802c53763d8561b962a5104f02ec09fad
Gamey001 <gamalieldashua@gmail.com>
2021-10-05 18:44:09 +0100

    Update notification.js

--- server/controllers/notification.js
+++ server/controllers/notification.js
@@ -5,87 +5,67 @@ exports.createNotification = asyncHandler(async (req, res, next) => {
   const { senderEmail, receipientEmail, type, title, description, read } =
     req.body;
 
-  try {
-    const notification = await Notification.create({
-      type,
-      title,
-      description,
-      read,
-      receipientEmail,
-      senderEmail,
-    });
+  const notification = await Notification.create({
+    type,
+    title,
+    description,
+    read,
+    receipientEmail,
+    senderEmail,
+  });
 
-    res.status(201).json({
-      success: {
-        notification: {
-          id: notification._id,
-          type: notification.type,
-          title: notification.title,
-          description: notification.description,
-          read: notification.read,
-          receipientEmail: notification.receipientEmail,
-          senderEmail: notification.senderEmail,
-        },
+  res.status(201).json({
+    success: {
+      notification: {
+        id: notification._id,
+        type: notification.type,
+        title: notification.title,
+        description: notification.description,
+        read: notification.read,
+        receipientEmail: notification.receipientEmail,
+        senderEmail: notification.senderEmail,
       },
-    });
-  } catch (e) {
-    res.status(500);
-    throw new Error("Something went wrong, please try again");
-  }
+    },
+  });
 });
 
 exports.markNotificationAsRead = asyncHandler(async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const notification = await Notification.findById(id);
+  const { id } = req.params;
+  const notification = await Notification.findById(id);
 
-    if (!natification) {
-      res.status(404);
-      throw new Error("The notification does not exist");
-    }
-    notification.toggleReadStatus();
-
-    res.status(200).json({
-      success: {
-        notification: {
-          id: notification._id,
-          description: notification.description,
-          read: notification.read,
-        },
-      },
-    });
-  } catch (e) {
-    res.status(500);
-    throw new Error("Something went wrong, please try again");
+  if (!natification) {
+    res.status(404);
+    throw new Error("The notification does not exist");
   }
+  notification.toggleReadStatus();
+
+  res.status(200).json({
+    success: {
+      notification: {
+        id: notification._id,
+        description: notification.description,
+        read: notification.read,
+      },
+    },
+  });
 });
 
 exports.getAllNotifications = asyncHandler(async (req, res, next) => {
-  try {
-    const notifications = await Notification.find();
+  const notifications = await Notification.find();
 
-    if (!natifications) return res.json({ msg: "No results found" });
+  if (!natifications) return res.json({ msg: "No results found" });
 
-    res.status(200).json({
-      success: notifications,
-    });
-  } catch (e) {
-    res.status(500);
-    throw new Error("Something went wrong, please try again");
-  }
+  res.status(200).json({
+    success: notifications,
+  });
 });
 
 exports.getUnreadNotifications = asyncHandler(async (req, res, next) => {
-  try {
-    const notifications = await Notification.find({ read: false });
+  const notifications = await Notification.find({ read: false });
 
-    if (!notification) return res.json({ msg: "No results found" });
+  if (!notification) return res.json({ msg: "No results found" });
 
-    res.status(200).json({
-      success: notifications,
-    });
-  } catch (e) {
-    res.status(500);
-    throw new Error("Something went wrong, please try again");
-  }
+  res.status(200).json({
+    success: notifications,
+  });
 });
