369970ea7f73d2f310dcca7655acf1817686d00b
Gamey001 <gamalieldashua@gmail.com>
2021-10-05 18:22:44 +0100

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,26 +1,25 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  registerDate: {
-    type: Date,
-    default: Date.now
-  }
-});
+  { timestamps: true }
+);
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
