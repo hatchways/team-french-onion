7a0c9cd3737d01ffdee5001677efa0ad4a6cda97
Gamey001 <87187530+Gamey001@users.noreply.github.com>
2021-10-08 09:30:16 +0100

    Update profile.js

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,7 +1,27 @@
 const express = require("express");
+const protect = require("../middleware/auth");
+const upload = require("../middleware/multer");
+const { validateMongoId, validateProfileDetails } = require("../validate");
 const router = express.Router();
-const { createProfile } = require("../controllers/profile");
+const {
+  updateProfile,
+  getProfile,
+  getAllProfiles,
+  uploadProfilePic,
+  createProfile
+} = require("../controllers/profile");
 
-router.route("/create").post(createProfile);
+router.route("/create").post(protect, validateProfileDetails, createProfile);
+
+router
+  .route("/update")
+  .put(validateMongoId, validateProfileDetails, updateProfile);
+
+router.route("/").get(protect, getProfile);
+
+router.route("/upload").post(upload.single("picture"), uploadProfilePic);
+
+router.route("/profiles").get(protect, getAllProfiles);
 
 module.exports = router;
+
