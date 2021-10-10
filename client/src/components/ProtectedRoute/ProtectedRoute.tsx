a84de55b817c32b7bbafe574f2dbe6abfd19b130
Gamey001 <gamalieldashua@gmail.com>
2021-10-10 17:52:23 +0100

    Update ProtectedRoute.tsx

--- client/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useAuth } from '../../context/useAuthContext';
 import { Route, Redirect } from 'react-router-dom';
-
 type Props = {
   children: JSX.Element;
   path: string;
@@ -9,23 +8,12 @@ type Props = {
 };
 const ProtectedRoute: React.FC<Props> = ({ children, ...restOfProps }) => {
   const { loggedInUser } = useAuth();
-  let isLoggedInAndVerified = false;
+  let isLoggedInAndValid = false;
 
-  if (loggedInUser) {
-    const { username } = loggedInUser;
-    if (username) {
-      fetch(`http://localhost:3001/searchUsers?search=${username}`)
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.users[0]) {
-            isLoggedInAndVerified = true;
-          }
-        })
-        .catch((err) => {
-          return <Redirect to="/login" />;
-        });
-    }
+  if (loggedInUser && loggedInUser.username && loggedInUser.email) {
+    isLoggedInAndValid = true;
   }
-  return <Route {...restOfProps} render={() => (isLoggedInAndVerified ? children : <Redirect to="/login" />)} />;
+
+  return <Route {...restOfProps} render={() => (isLoggedInAndValid ? children : <Redirect to="/login" />)} />;
 };
 export default ProtectedRoute;
