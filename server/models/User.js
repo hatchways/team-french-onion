c5ba64ab91ffad182504e5b2b8ced50bdf1a657e
Gamey001 <gamalieldashua@gmail.com>
2021-10-12 11:57:51 +0100

    Revert "Merge branch 'main' into protected-route"
    This reverts commit c6ba435c8a3a46aece0e297415b90dd2ec24a9e0, reversing
    changes made to a84de55b817c32b7bbafe574f2dbe6abfd19b130.

--- server/models/User.js
+++ server/models/User.js
@@ -1,25 +1,22 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
   },
-  { timestamps: true }
-);
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+}, {timestamps: true},);
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
--- server/validate.js
+++ server/validate.js
@@ -1,17 +1,13 @@
 const { check, validationResult } = require("express-validator");
 
-const msgPrefix = "Please enter a valid";
-const genderOptions = ["MALE", "FEMALE", "OTHER"];
-const checkFalsyVal = { checkFalsy: true };
-
 exports.validateRegister = [
-  check("username", `${msgPrefix} username`).notEmpty(),
-  check("email", `${msgPrefix} email address`).isEmail(),
+  check("username", "Please enter a username").not().isEmpty(),
+  check("email", "Please enter a valid email address").isEmail(),
   check(
     "password",
     "Please enter a password with 6 or more characters"
   ).isLength({
-    min: 6,
+    min: 6
   }),
   (req, res, next) => {
     const errors = validationResult(req);
@@ -20,79 +16,17 @@ exports.validateRegister = [
     if (!errors.isEmpty())
       return res.status(400).json({ errors: errors.array() });
     next();
-  },
+  }
 ];
 
 exports.validateLogin = [
-  check("email", `${msgPrefix} email address`).isEmail(),
-  check("password", "Password is required").notEmpty(),
+  check("email", "Please enter a valid email address").isEmail(),
+  check("password", "Password is required").not().isEmpty(),
   (req, res, next) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty())
       return res.status(400).json({ errors: errors.array() });
     next();
-  },
-];
-
-exports.validateProfileDetails = [
-  check("firstName", ` ${msgPrefix} first name`)
-    .exists(checkFalsyVal)
-    .trim()
-    .escape()
-    .isLength({ max: 20 }),
-  check("lastName", ` ${msgPrefix} last name`)
-    .exists(checkFalsyVal)
-    .trim()
-    .escape()
-    .isLength({ max: 20 }),
-  check("gender", ` ${msgPrefix} gender`)
-    .exists(checkFalsyVal)
-    .isIn(genderOptions),
-  check("birthday", ` ${msgPrefix} birthday format`)
-    .exists(checkFalsyVal)
-    .toDate(),
-  check("email", ` ${msgPrefix} email address`)
-    .exists(checkFalsyVal)
-    .trim()
-    .escape()
-    .isEmail(),
-  check("phoneNumber", ` ${msgPrefix} phone number`)
-    .exists(checkFalsyVal)
-    .trim()
-    .isMobilePhone(),
-  check("location", ` ${msgPrefix} location`)
-    .exists(checkFalsyVal)
-    .trim()
-    .escape()
-    .isLength({ max: 10 }),
-  check("profilePic", ` ${msgPrefix} image url`)
-    .exists(checkFalsyVal)
-    .escape()
-    .isURL(),
-  check("description", ` ${msgPrefix} description format`)
-    .exists(checkFalsyVal)
-    .escape()
-    .isLength({ max: 100 }),
-  check("availability", ` ${msgPrefix} availability format`)
-    .exists(checkFalsyVal)
-    .isArray(),
-  (req, res, next) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty())
-      return res.status(400).json({ errors: errors.array() });
-    next();
-  },
-];
-
-exports.validateMongoId = [
-  check("_id", "Invalid profile ID").exists(checkFalsyVal).isMongoId(),
-  (req, res, next) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty())
-      return res.status(400).json({ errors: errors.array() });
-    next();
-  },
+  }
 ];
