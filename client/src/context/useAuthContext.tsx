4041e4945dc3eb2d15c1fc82a838ac81fd761333
Gamey001 <gamalieldashua@gmail.com>
2021-10-13 23:21:11 +0100

    Update useAuthContext.tsx

--- client/src/context/useAuthContext.tsx
+++ client/src/context/useAuthContext.tsx
@@ -50,11 +50,11 @@ export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
         } else {
           // don't need to provide error feedback as this just means user doesn't have saved cookies or the cookies have not been authenticated on the backend
           setLoggedInUser(null);
-          //history.push('/login');
+          history.push('/login');
         }
       });
     };
-    // checkLoginWithCookies();
+    checkLoginWithCookies();
   }, [updateLoginContext, history]);
 
   return <AuthContext.Provider value={{ loggedInUser, updateLoginContext, logout }}>{children}</AuthContext.Provider>;
