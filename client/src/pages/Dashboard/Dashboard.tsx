4194a79b1da86375b7e516fee04d7441e269405a
Jason <xyecs@users.noreply.github.com>
2021-10-12 22:15:26 -0400

    profile listings

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -5,11 +5,12 @@ import useStyles from './useStyles';
 import { useAuth } from '../../context/useAuthContext';
 import { useSocket } from '../../context/useSocketContext';
 import { useHistory } from 'react-router-dom';
-import ChatSideBanner from '../../components/ChatSideBanner/ChatSideBanner';
 import { useEffect } from 'react';
-import logo from '../../Images/68f55f7799df6c8078a874cfe0a61a5e6e9e1687.png';
-import LogoHeader from '../../components/LogoHeader/LogoHeader';
 import NavBar from '../../components/NavBar/NavBar';
+import { Typography } from '@material-ui/core';
+import DashboardSearch from './DashboardSearch/DashboardSearch';
+import ProfileCard from '../../components/ProfileCard/ProfileCard';
+import { mockProfiles } from '../../mocks/mockProfile';
 
 export default function Dashboard(): JSX.Element {
   const classes = useStyles();
@@ -18,6 +19,10 @@ export default function Dashboard(): JSX.Element {
   const { initSocket } = useSocket();
 
   const history = useHistory();
+
+  const handleSubmit = () => {
+    console.log('submitted');
+  };
 
   useEffect(() => {
     initSocket();
@@ -34,8 +39,18 @@ export default function Dashboard(): JSX.Element {
     <Grid container component="main" className={`${classes.root} ${classes.dashboard}`}>
       <CssBaseline />
       <NavBar loggedIn user={loggedInUser} />
-      <Grid item className={classes.drawerWrapper}>
-        <ChatSideBanner loggedInUser={loggedInUser} />
+      <Grid item className={classes.searchWrapper}>
+        <Typography className={classes.searchText}>Search for Users</Typography>
+        <DashboardSearch />
+      </Grid>
+
+      {/**Currently using mock profiles, need to change to actual list of profiles from api call */}
+      <Grid container spacing={5} className={classes.listingsWrapper}>
+        {Array.from(mockProfiles).map((_, index) => (
+          <Grid item xs={2} sm={4} md={4} key={index} className={classes.listingItem}>
+            <ProfileCard profile={mockProfiles[index]}></ProfileCard>
+          </Grid>
+        ))}
       </Grid>
     </Grid>
   );
