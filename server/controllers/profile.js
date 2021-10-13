765edf5ae1b9f1ce3eee6b1e9f203dfabf151adf
Gamey001 <gamalieldashua@gmail.com>
2021-10-14 00:23:15 +0100

    Update profile.js

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -1,12 +1,11 @@
 const Profile = require("../models/Profile");
-const { cloudinary } = require("../utils/cloudinary");
 const asyncHandler = require("express-async-handler");
 
 // @route POST /profile/create
 // @desc Create new profile
 // @access Public
 exports.createProfile = asyncHandler(async (req, res, next) => {
-  try {
+
     const {
       firstName,
       lastName,
@@ -19,6 +18,7 @@ exports.createProfile = asyncHandler(async (req, res, next) => {
       description,
       availability,
     } = req.body;
+    
     const profile = await Profile.create({
       firstName,
       lastName,
@@ -35,17 +35,12 @@ exports.createProfile = asyncHandler(async (req, res, next) => {
     res.status(201).json({
       profile,
     });
-  } catch (err) {
-    res.status(500);
-    throw new Error("Something went wrong, please try again");
-  }
 });
 
 // @route PUT /profile
-// @desc update a profile with given ID
+// @desc updates a profile with given ID
 // @access Private
 exports.updateProfile = asyncHandler(async (req, res, next) => {
-  try {
     const id = req.user._id;
 
     const {
@@ -60,6 +55,7 @@ exports.updateProfile = asyncHandler(async (req, res, next) => {
       description,
       availability,
     } = req.body;
+
     const profile = await Profile.findOneAndUpdate(
       { user: id },
       {
@@ -76,62 +72,41 @@ exports.updateProfile = asyncHandler(async (req, res, next) => {
       },
       { new: true }
     );
+
     res.status(200).json({
       profile,
     });
-  } catch (err) {
-    res.status(500);
-    throw new Error("Something went wrong, please try again");
-  }
 });
 
 // @route GET /profile
 // @desc gets a profile with the given ID
 // @access Private
 exports.getProfile = asyncHandler(async (req, res, next) => {
-  try {
     const id = req.user._id;
     const profile = await Profile.findOne({ user: id });
 
     if (!profile) {
       res.status(404);
-      throw new Error("This profile does not exist");
+      throw new Error("The profile does not exist");
     }
+
     res.status(200).json({
       profile,
     });
-  } catch (err) {
-    res.status(500);
-    throw new Error("Something went wrong, please try again");
-  }
 });
 
-// @route GET /profile
-// @desc get all profiles
+// @route GET /profiles
+// @desc gets all profiles
 // @access Private
 exports.getAllProfiles = asyncHandler(async (req, res, next) => {
     const profiles = await Profile.find();
+
+    if (!profiles) {
+      res.status(500);
+      throw new Error("0 results");
+    }
+
     res.status(200).json({
       profiles,
     });
-});
-
-exports.uploadProfilePic = asyncHandler(async (req, res, next) => {
-    const { _id, file } = req.user;
-    const profile = await Profile.find({ user: _id });
-
-    if (!file) {
-      res.status(400);
-      throw new Error("Failed to upload photo, ensure that you have selected a valid file format")
-    }
-      const { secure_url } = await cloudinary.uploader.upload(file.path, {
-        folder: "dogSittersAndOwnersPhotos"
-      });
-
-      profile.addPhoto(secure_url, "profilePic");
-      profile.save();
-  
-      res.status(200).json({
-        message: "Image uploaded successfully",
-      }); 
 });
