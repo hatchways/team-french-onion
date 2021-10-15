cf6396ff9687c451df4962daca56dbebb293939d
Jason <xyecs@users.noreply.github.com>
2021-10-14 20:15:25 -0400

    requested changes

--- client/src/components/AuthMenu/AuthMenu.tsx
+++ client/src/components/AuthMenu/AuthMenu.tsx
@@ -1,22 +1,15 @@
 import { useState, MouseEvent } from 'react';
-import IconButton from '@material-ui/core/IconButton';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import { useAuth } from '../../context/useAuthContext';
-import AvatarDisplay from '../AvatarDisplay/AvatarDisplay';
 import { User } from '../../interface/User';
 import Avatar from '@material-ui/core/Avatar';
 
-interface Props {
-  loggedIn: boolean;
-  user?: User;
-}
-
-const AuthMenu = ({ user }: Props): JSX.Element => {
+const AuthMenu = (): JSX.Element => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const { logout } = useAuth();
+  const { loggedInUser } = useAuth();
 
   const handleClick = (event: MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -39,7 +32,7 @@ const AuthMenu = ({ user }: Props): JSX.Element => {
         aria-haspopup="true"
         onClick={handleClick}
         alt="Profile Image"
-        src={`https://robohash.org/${user?.email}.png`}
+        src={`https://robohash.org/${loggedInUser?.email}.png`}
       />
       <Menu
         id="auth-menu"
