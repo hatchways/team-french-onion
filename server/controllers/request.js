94eae980d79a1ed7a7fc364a3081c1befe0f87fa
Jason <xyecs@users.noreply.github.com>
2021-10-15 10:15:07 -0400

    fix conflict

--- server/controllers/request.js
+++ server/controllers/request.js
@@ -1,5 +1,6 @@
 const Request = require("../models/Request");
 const asyncHandler = require("express-async-handler");
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 // @route GET /requests
 // @desc Search for request of especific userId
@@ -58,4 +59,36 @@ exports.updateRequest = asyncHandler(async (req, res, next) => {
     res.status(400);
     throw new Error("Invalid user Id data");
   }
+});
+
+// @route PATH /requests/:requestId/pay
+// @desc Update pay status
+// @access Private
+
+exports.payRequest = asyncHandler(async (req, res, next) => {
+  const requestId = req.params.requestId;
+  const request = await Request.findOne({ _id: requestId });
+  const { items } = req.body;
+
+  const todayDate = new Date();
+
+  if (todayDate > request.end) {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: calculateOrderAmount(items),
+      currency: "cad",
+    });
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Current date has to be after end date of request to pay.");
+  }
+
+  const calculateOrderAmount = (items) => {
+    // Replace this constant with a calculation of the order's amount
+    // Calculate the order total on the server to prevent
+    // people from directly manipulating the amount on the client
+    return 1400;
+  };
 });
--- server/routes/profile.js
+++ server/routes/profile.js
@@ -9,16 +9,14 @@ const {
   createProfile,
 } = require("../controllers/profile");
 
+router.route("/").get(protect, getProfile);
+
 router.route("/create").post(validateProfileDetails, createProfile);
 
 router
   .route("/update")
   .put(protect, validateMongoId, validateProfileDetails, updateProfile);
 
-router.route("/").get(protect, getProfile);
-
 router.route("/profiles").get(protect, getAllProfiles);
-const router = express.Router();
-const { createProfile } = require("../controllers/profile");
 
 module.exports = router;
